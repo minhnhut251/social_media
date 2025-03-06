bf2d02dab3b4f2f273f3bf459c3e3bee8f60c76b
nhut <minhnhut251s@gmail.com>
2025-03-06 16:50:31 +0700

    update and delete post

--- src/main/java/com/da2/socialmedia/controller/PostController.java
+++ src/main/java/com/da2/socialmedia/controller/PostController.java
@@ -11,9 +11,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -51,31 +53,28 @@ public class PostController {
         return "redirect:/";
     }
 
-//    @GetMapping("/edit_post/{id}")
-//    public String showUpdateForm(@PathVariable("id") long id, Model model) {
-//        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//        model.addAttribute("user", user);
-//
-//        return "update-user";
-//    }
-//
-//    @PostMapping("/update/{id}")
-//    public String updateUser(@PathVariable("id") long id, @Valid User user, BindingResult result, Model model) {
-//        if (result.hasErrors()) {
-//            user.setId(id);
-//            return "update-user";
-//        }
-//
-//        userRepository.save(user);
-//
-//        return "redirect:/index";
-//    }
-//
-//    @GetMapping("/delete/{id}")
-//    public String deleteUser(@PathVariable("id") long id, Model model) {
-//        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-//        userRepository.delete(user);
-//
-//        return "redirect:/index";
-//    }
+    @GetMapping("/post_edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        PostEntity post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        model.addAttribute("post", post);
+
+        return "edit-post";
+    }
+
+    @PostMapping("/update_post/{id}")
+    public String updateUser(@PathVariable("id") long id, PostEntity post, Model model) {
+        PostEntity existing = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        existing.setNoiDung(post.getNoiDung());
+        postRepository.save(existing);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/post_delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        PostEntity post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        postRepository.delete(post);
+
+        return "redirect:/";
+    }
 }
