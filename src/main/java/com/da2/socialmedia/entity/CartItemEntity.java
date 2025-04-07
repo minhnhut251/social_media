f1342de8518fd3c3e6d1e203b4a3a0a7f87c12d0
nhut <minhnhut251s@gmail.com>
2025-04-07 11:41:39 +0700

    sua gio hang

--- src/main/java/com/da2/socialmedia/entity/CartItemEntity.java
+++ src/main/java/com/da2/socialmedia/entity/CartItemEntity.java
@@ -37,37 +37,37 @@ public class CartItemEntity{
         this.product = product;
         this.quantity = quantity;
     }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public SanphamEntity getProduct() {
-        return product;
-    }
-
-    public void setProduct(SanphamEntity product) {
-        this.product = product;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
+//
+//    // Getters and Setters
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+//
+//    public SanphamEntity getProduct() {
+//        return product;
+//    }
+//
+//    public void setProduct(SanphamEntity product) {
+//        this.product = product;
+//    }
+//
+//    public Integer getQuantity() {
+//        return quantity;
+//    }
+//
+//    public void setQuantity(Integer quantity) {
+//        this.quantity = quantity;
+//    }
 }
